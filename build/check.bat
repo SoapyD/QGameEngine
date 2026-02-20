d5ee046436b82c8872cdf645249ebf70ef381d03
t-cassady <tjc.software.ltd@gmail.com>
2026-02-20 23:36:27 +0000

    added tutorial 08

--- build/check.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d D:\Documents\Programming\C_Projects\QEngine
-C:\msys64\ucrt64\bin\g++.exe -std=gnu++20 -Isrc -Iextern/entt/single_include -Iextern/glfw/include -Iextern/glad/include -Iextern/glm -Iextern/stb -fsyntax-only src/engine/ecs/scene_setup.cpp > build\check_out.txt 2>&1
-echo EXIT: %ERRORLEVEL% >> build\check_out.txt
--- build/check2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d D:\Documents\Programming\C_Projects\QEngine
-C:\msys64\ucrt64\bin\g++.exe -std=gnu++20 -Isrc -Iextern\entt\single_include -Iextern\glfw\include -Iextern\glad\include -Iextern\glm -Iextern\stb -fsyntax-only src\engine\ecs\scene_setup.cpp 1>build\stdout.txt 2>build\stderr.txt
-echo EXIT: %ERRORLEVEL% >build\exitcode.txt
